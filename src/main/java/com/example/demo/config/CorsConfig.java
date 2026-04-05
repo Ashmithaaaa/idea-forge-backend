0d6662a618e745857eafd635519b622d8f89a8d3
Ashmitha <ashmithareddy2005@gmail.com>
2026-04-05 20:15:26 +0530

    fix cors issue

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("*") // 🔥 FIX HERE
+                        .allowedOrigins("https://idea-forge-frontend-nine.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
